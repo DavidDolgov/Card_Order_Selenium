50597975d246006801180f107347ffc8685fd877
Dolgov David <dolgov.david.1993@gmail.com>
2023-06-15 03:28:18 +0300

    finalization of tests

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -4,12 +4,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,7 +15,6 @@ public class CardOrderTest {
 
     @BeforeAll
     static void setUpAll() {
-        // System.setProperty("webdriver.chrome.driver", ".driver/win/chromedriver.exe");
         WebDriverManager.chromedriver().setup();
     }
 
@@ -30,6 +26,7 @@ public class CardOrderTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
@@ -40,7 +37,7 @@ public class CardOrderTest {
 
     @Test
     void happyPathTest1() {
-        driver.get("http://localhost:9999");
+
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Долгов Давид");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79998887766");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
@@ -52,14 +49,41 @@ public class CardOrderTest {
     }
 
     @Test
-    void negativePathTest1() {
-        driver.get("http://localhost:9999");
+    void negativePathTestForInputFieldName() {
+
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Dolgov David");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79998887766");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.tagName("button")).click();
-        List<WebElement> elements = driver.findElements(By.cssSelector("[data-test-id=name] span"));
         String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        String actual = elements.get(3).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=name] .input__sub")).getText().trim();
         assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void negativePathTestForInputFieldPhone() {
+
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Долгов Давид");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+7999888776611111111");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.tagName("button")).click();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void negativePathTestForCheckbox() {
+
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Долгов Давид");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79998887766");
+        driver.findElement(By.tagName("button")).click();
+        String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=agreement] .checkbox__text")).getText().trim();
+        assertEquals(expected, actual);
+
     }
 
 }
